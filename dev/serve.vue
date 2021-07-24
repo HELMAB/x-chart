552f0b5fcb9f9d25c4f1dd255892f5d3c539b9ad
HEL Mab <mabhelitc@gmail.com>
2021-07-24 20:59:52 +0700

    update pakcage

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { XChartSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  XChartSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <x-chart-sample />
+  </div>
+</template>
